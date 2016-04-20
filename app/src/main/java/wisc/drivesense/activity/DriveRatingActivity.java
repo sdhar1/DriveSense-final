e9e0084fea6f33eaafe10c9a0537ecb7c0f7f3b8
sdhar1 <sdhar@cs.wisc.edu>
2016-04-20 01:35:43 -0500

    Added Back button

--- app/src/main/java/wisc/drivesense/activity/DriveRatingActivity.java
+++ app/src/main/java/wisc/drivesense/activity/DriveRatingActivity.java
@@ -1,11 +1,14 @@
 package wisc.drivesense.activity;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 
 import wisc.drivesense.R;
@@ -16,12 +19,22 @@ public class DriveRatingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        Trip trp = (Trip)intent.getSerializableExtra("Current Trip");
-        //***** Add trp to DB *****
         setContentView(R.layout.activity_drive_rating);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        Trip trp = (Trip) intent.getSerializableExtra("Current Trip");
+        //***** Add trp to DB *****
+
+        Toolbar ratingToolbar = (Toolbar) findViewById(R.id.tool_bar_rating);
+
+        ratingToolbar.setTitle("Your Drive Rating");
+        ratingToolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        ratingToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +45,4 @@ public class DriveRatingActivity extends AppCompatActivity {
             }
         });
     }
-
 }
--- app/src/main/java/wisc/drivesense/activity/HistoryActivity.java
+++ app/src/main/java/wisc/drivesense/activity/HistoryActivity.java
@@ -1,9 +1,17 @@
 package wisc.drivesense.activity;
 
+import android.app.ActionBar;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import wisc.drivesense.R;
 
@@ -12,11 +20,50 @@ public class HistoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_history);
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.historytoolbar);
+        toolbar.setTitle("Previous Trips");
+        toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         ListView listView = (ListView)findViewById(R.id.listView);
         //*****To be replaced by list of trips*****
         String[] trips= {"trip1","trip2","trip3","trip4","trip5","trip6"};
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,trips);
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.basic_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+
+                return true;
+
+            case R.id.home:
+                this.finish();
+                return true;
+            case R.id.about:
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
