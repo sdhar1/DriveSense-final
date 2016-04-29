6a73366414f24ba963decfdac3aae2da32419b2c
kanglei1130 <kanglei1130@gmail.com>
2016-04-29 18:37:25 -0500

    map dots

--- app/src/main/java/wisc/drivesense/activity/MapActivity.java
+++ app/src/main/java/wisc/drivesense/activity/MapActivity.java
@@ -2,8 +2,15 @@ package wisc.drivesense.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.LevelListDrawable;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,6 +19,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
@@ -26,9 +34,10 @@ import wisc.drivesense.utility.Trip;
 
 public class MapActivity extends Activity implements OnMapReadyCallback {
 
-    static final LatLng madison = new LatLng(43.073052 , -89.401230);
+    static final LatLng madison_ = new LatLng(43.073052 , -89.401230);
     //private GoogleMap map;
-    private Trip trip;
+    private Trip trip_;
+    private static String TAG = "MapActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +45,7 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         setContentView(R.layout.activity_map);
 
         Intent intent = getIntent();
-        trip = (Trip) intent.getSerializableExtra("Current Trip");
+        trip_ = (Trip) intent.getSerializableExtra("Current Trip");
         Toolbar ratingToolbar = (Toolbar) findViewById(R.id.tool_bar_rating);
 
         ratingToolbar.setTitle("Your Trip");
@@ -49,7 +58,7 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         });
 
         TextView ratingView = (TextView) findViewById(R.id.rating);
-        ratingView.setText(String.format("%.1f", trip.getScore()));
+        ratingView.setText(String.format("%.1f", trip_.getScore()));
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -66,14 +75,15 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         map.setBuildingsEnabled(true);
         map.getUiSettings().setZoomControlsEnabled(true);
 
-        LatLng destination;
-        if(trip.getGPSPoints().size() >= 2) {
-            destination = new LatLng(trip.getEndPoint().values[0], trip.getEndPoint().values[1]);
+        LatLng start;
+        int sz = trip_.getGPSPoints().size();
+        if(sz >= 2) {
+            start = new LatLng(trip_.getStartPoint().values[0], trip_.getStartPoint().values[1]);
         } else {
-            destination = madison;
+            start = madison_;
         }
         CameraPosition position = CameraPosition.builder()
-                .target(destination)
+                .target(start)
                 .zoom( 15f )
                 .bearing( 0.0f )
                 .tilt( 0.0f )
@@ -81,19 +91,34 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
 
         map.moveCamera(CameraUpdateFactory.newCameraPosition(position));
 
-
-        plotRoute(map);
+        if(sz >= 2) {
+            plotRoute(map);
+        }
     }
 
 
     private void plotRoute(final GoogleMap map) {
 
-        List<Trace> gps = trip.getGPSPoints();
-        int sz = gps.size();
+        List<Trace> gps = trip_.getGPSPoints();
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+
+        Bitmap bmp = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmp);
+
+        Paint paint = new Paint();
+        paint.setColor(0xFFFF0000);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setStrokeWidth(4.5f);
+        canvas.drawCircle(50, 50, 30, paint);
+
+
+
         for (int i = 0; i < gps.size(); i++) {
             Trace point = gps.get(i);
-            Marker marker = map.addMarker(new MarkerOptions().position(new LatLng(point.values[0], point.values[1])));
+            //.icon(BitmapDescriptorFactory.fromResource(R.drawable.buttondefault)
+            MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(point.values[0], point.values[1])).icon(BitmapDescriptorFactory.fromBitmap(bmp));
+            Marker marker = map.addMarker(markerOptions);
             builder.include(marker.getPosition());
         }
         final LatLngBounds bounds = builder.build();
